b9cefbe9706c9dcfbf50984333e5b0d8aa955314
ziv <ziv50109@gmail.com>
2017-12-06 23:03:06 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,77 @@
+jQuery(function($) {'use strict';
+
+	//Initiat WOW JS
+	new WOW().init();
+
+	// portfolio filter
+	$(window).load(function(){
+
+		$('.main-slider').addClass('animate-in');
+		$('.preloader').remove();
+		//End Preloader
+
+		if( $('.masonery_area').length ) {
+			$('.masonery_area').masonry();//Masonry
+		}
+
+		var $portfolio_selectors = $('.portfolio-filter >li>a');
+
+		if($portfolio_selectors.length) {
+
+			var $portfolio = $('.portfolio-items');
+			$portfolio.isotope({
+				itemSelector : '.portfolio-item',
+				layoutMode : 'fitRows'
+			});
+
+			$portfolio_selectors.on('click', function(){
+				$portfolio_selectors.removeClass('active');
+				$(this).addClass('active');
+				var selector = $(this).attr('data-filter');
+				$portfolio.isotope({ filter: selector });
+				return false;
+			});
+		}
+
+	});
+
+	// Contact form
+	var form = $('#main-contact-form');
+	form.submit(function(event){
+		event.preventDefault();
+		var form_status = $('<div class="form_status"></div>');
+		$.ajax({
+			url: $(this).attr('action'),
+			beforeSend: function(){
+				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+			}
+		}).done(function(data){
+			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+		});
+	});
+
+	if( $('#gmap').length ) {
+		var map;
+
+		map = new GMaps({
+			el: '#gmap',
+			lat: 25.046302,
+			lng: 121.469514,
+			scrollwheel: true,
+			zoom: 17,
+			zoomControl : false,
+			panControl : false,
+			streetViewControl : false,
+			mapTypeControl: false,
+			overviewMapControl: false,
+			clickable: false
+		});
+
+		map.addMarker({
+			lat: 25.046282,
+			lng: 121.4678014,
+			animation: google.maps.Animation.DROP
+		});
+	}
+
+});
